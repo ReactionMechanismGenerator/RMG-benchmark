c1c7693fe308c954a9148fce0da3a4ce36fbc6d4
JacksonBurns <jwburns@mit.edu>
2026-02-27 13:57:54 -0500

    add patch for numproc calculation

--- 2.4.1/install.sh
+++ 2.4.1/install.sh
@@ -3,6 +3,9 @@ cd RMG-Py
 git checkout af0ef48bda472c4689605015ac0adda102425aae
 cd ..
 
+# patch out the automatic multiprocessing code which doesn't work on our particular hardware
+sed -i 's/psutil\.virtual_memory().free/psutil.virtual_memory().available/' RMG-Py/rmgpy/rmg/main.py
+
 git clone git@github.com:ReactionMechanismGenerator/RMG-database.git
 cd RMG-database
 git checkout d52ebabb3478377660dd4c25c938844549d9ec42
